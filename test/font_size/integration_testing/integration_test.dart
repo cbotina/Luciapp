86cbba2eac5325eebd5551720f2c558ef8a09fa7
Carlos Botina <carlosalberto.botinacarpio@gmail.com>
2024-02-06 11:20:30 -0500

    Setup integration test file for font_size feature

--- test/font_size/integration_testing/integration_test.dart
+++ test/font_size/integration_testing/integration_test.dart
@@ -1,90 +1,63 @@
-import 'dart:async';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:luciapp/common/constants/widget_keys.dart';
 import 'package:luciapp/features/auth/application/auth_service.dart';
-import 'package:luciapp/features/auth/data/providers/is_logged_in_provider.dart';
 import 'package:luciapp/features/auth/domain/enums/auth_method.dart';
 import 'package:luciapp/features/auth/domain/enums/auth_result.dart';
-import 'package:luciapp/features/auth/domain/enums/gender.dart';
-import 'package:luciapp/features/auth/domain/models/user.dart';
 import 'package:luciapp/features/font_size/domain/models/user_font_settings.dart';
 import 'package:luciapp/features/font_size/presentation/controllers/font_size_controller.dart';
 import 'package:luciapp/features/themes/application/theme_service.dart';
-import 'package:luciapp/features/themes/domain/enums/app_theme_mode.dart';
-import 'package:luciapp/features/themes/presentation/controllers/theme_controller.dart';
 import 'package:luciapp/features/themes/presentation/state/theme_state.dart';
 import 'package:luciapp/main.dart';
 import 'package:mocktail/mocktail.dart';
-
-import '../../auth/mocks/mock_auth_controller.dart';
 import '../../auth/mocks/mock_auth_service.dart';
 import '../../common/mocks/mock_auth_repository.dart';
 import '../../common/robot/testing_robot.dart';
 import '../../themes/mocks/mock_theme_service.dart';
-import '../../themes/mocks/mock_theme_settings_repository.dart';
 import '../mocks/mock_font_settings_repository.dart';
-import '../mocks/mock_font_size_service.dart';
 
 void main() {
-  late final User testUser;
   late final MockAuthRepository mockAuthRepository;
-  late final MockAuthController mockAuthController;
   late final MockAuthService mockAuthService;
   late final MockThemeService mockThemeService;
-  late final MockFontSizeService mockFontSizeService;
-  late final MockThemeSettingsRepository mockThemeSettingsRepository;
 
-  late bool darkmode;
-  late bool hcmode;
+  late final MockFontSettingsRepository mockFontSettingsRepository;
 
   setUpAll(() async {
     mockAuthService = MockAuthService();
     mockThemeService = MockThemeService();
     mockAuthRepository = MockAuthRepository();
-    mockFontSizeService = MockFontSizeService();
-    mockThemeSettingsRepository = MockThemeSettingsRepository();
+    mockFontSettingsRepository = MockFontSettingsRepository();
+    registerFallbackValue(UserFontSettings.initial('1234'));
 
     when(() => mockAuthService.login(AuthMethod.facebook)).thenAnswer(
       (_) => Future.value(AuthResult.success),
     );
 
-    // when(() => mockThemeSettingsRepository.get('1234')).thenAnswer(
-    //   (invocation) {
-    //     return Future.delayed?(UserFontSettings.initial('1234'));
-    //   },
-    // );
-
-    when(mockThemeService.toggleHCMode).thenAnswer((_) {
-      hcmode = !hcmode;
-      return Future.value(
-          ThemeState(isDarkModeEnabled: darkmode, isHCModeEnabled: hcmode));
-    });
-
     when(mockThemeService.getCurrentThemeState).thenAnswer(
-      (_) => Future.value(
-        ThemeState(
-          isDarkModeEnabled: darkmode,
-          isHCModeEnabled: hcmode,
-        ),
-      ),
+      (_) => Future.value(const ThemeState.light()),
     );
 
+    when(() => mockFontSettingsRepository.get('1234')).thenAnswer(
+        (_) => Future.value(UserFontSettings(scaleFactor: 1, userId: '1234')));
+
     when(() => mockAuthService.getUserId()).thenReturn('1234');
+
+    when(() => mockAuthRepository.userId).thenAnswer((_) => '1234');
+
+    when(() => mockFontSettingsRepository.update(any()))
+        .thenAnswer((invocation) => Future.value(true));
   });
 
   final overrides = [
     authServiceProvider.overrideWith((ref) => mockAuthService),
     themeServiceProvider.overrideWith((ref) => mockThemeService),
     authRepositoryProvider.overrideWith((ref) => mockAuthRepository),
+    fontSettingsRepositoryProvider
+        .overrideWith((ref) => mockFontSettingsRepository),
   ];
 
   group("IntegrationTest", () {
     testWidgets("First", (WidgetTester tester) async {
-      darkmode = false;
-      hcmode = true;
-
       await tester.pumpWidget(
         ProviderScope(
           overrides: overrides,
@@ -107,15 +80,8 @@ void main() {
           .increaseFontSize();
 
       expect(
-        container.read(themeControllerProvider).value!.appThemeMode,
-        AppThemeMode.hcDark,
-      );
-
-      await container.read(themeControllerProvider.notifier).toggleDarkMode();
-
-      expect(
-        container.read(themeControllerProvider).value!.appThemeMode,
-        AppThemeMode.hcLight,
+        container.read(fontSizeControllerProvider).value!.scaleFactor,
+        1.1,
       );
     });
   });
